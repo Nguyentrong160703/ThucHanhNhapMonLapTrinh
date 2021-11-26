28e64f333eeda1e640e63ad366a36c9502b38b00
Nguyễn Anh Trọng <92981684+Nguyentrong160703@users.noreply.github.com>
2021-11-26 17:04:22 +0700

    BTVN b1

--- 2001210175_NguyenAnhTrong_b1_bai15_BTVN.cpp
+++ 2001210175_NguyenAnhTrong_b1_bai15_BTVN.cpp
@@ -3,28 +3,41 @@
 
 int main()
 {
-    int cot1,cot2,cot3,cot4, hs, c1,c2,c3,c4;
+    int cot1,cot2,cot3,cot4, hs=10;
     float tb;
     printf("\nNhap diem cot 1: ");
     scanf("%d",&cot1);
-    printf("Nhap he so cot 1: ");
-    scanf("%d",&c1);
     printf("\nNhap diem cot 2: ");
     scanf("%d",&cot2);
-    printf("Nhap he so cot 2: ");
-    scanf("%d",&c2);
     printf("\nNhap diem cot 3: ");
     scanf("%d",&cot3);
-    printf("Nhap he so cot 3: ");
-    scanf("%d",&c3);
     printf("\nNhap diem cot 4: ");
     scanf("%d",&cot4);
-    printf("Nhap he so cot 4: ");
-    scanf("%d",&c4);
-    hs=c1+c2+c3+c4;
-    tb=(float)((cot1*c1)+(cot2*c2)+(cot3*c3)+(cot4*c4))/hs;
-    printf("\n\nDiem trung binh cua 4 cot la: %.2f",tb);
+    tb=(float)((cot1+cot2+cot3+cot4))/4;
+    if (tb>=8.5&&tb<=10)
+	{
+		printf("diem tb cua sinh vien la %.2f\n", tb);
+		printf("diem chu cua sinh vien la %c\n", '\x41');
+	}
+	if (tb>=7 && tb<=8.4)
+	{
+		printf("diem tb cua sinh vien la %.2f\n", tb);
+		printf("diem chu cua sinh vien la %c\n", '\x42');
 
-    getch();
-
-    }
+	}
+	if (tb >= 5.5 && tb <= 6.9)
+	{
+		printf("diem tb cua sinh vien la %.2f\n", tb);
+		printf("diem chu cua sinh vien la %c\n", '\x43');
+	}
+	if (tb >= 4 && tb <= 5.4)
+	{
+		printf("diem tb cua sinh vien la %.2f\n", tb);
+		printf("diem chu cua sinh vien la %c\n", '\x44');
+	}
+	if (tb<4)
+	{
+		printf("diem tb cua sinh vien la %.2f\n", tb);
+		printf("diem chu cua sinh vien la %c\n", '\x46');
+	}
+}
